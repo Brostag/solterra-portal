980cf16ae6fa8d02a1697eebe8dba92f0a686ad0
Roberto Carrasco <roberto.carrasco.cerda@gmail.com>
2026-06-11 20:35:02 -0400

    chore: allow cloudflare tunnel dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["*.trycloudflare.com"],
   serverExternalPackages: ["@react-pdf/renderer", "@prisma/client"],
   images: {
     unoptimized: true,
